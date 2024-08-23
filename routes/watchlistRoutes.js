07ec36489717a58b6a0e14e54fff457f2d6a8ee8
Arthur Andrade <arthurandrad.23@gmail.com>
2024-08-23 15:26:24 -0300

    Feat: Added removeFromWishlist route

--- routes/watchlistRoutes.js
+++ routes/watchlistRoutes.js
@@ -23,4 +23,22 @@ router.post('/movies', authMiddleware, async (req, res) => {
   }
 });
 
+router.delete('/watchlist/movies', authMiddleware, async (req, res) => {
+    const { movieId } = req.body;
+  
+    try {
+      const user = await User.findById(req.user._id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      user.watchlist = user.watchlist.filter(movie => movie._id.toString() !== movieId);
+      await user.save();
+  
+      res.status(200).json({ message: 'Movie removed from watchlist', watchlist: user.watchlist });
+    } catch (error) {
+      res.status(500).json({ message: 'Failed to remove movie from watchlist', error });
+    }
+  });
+
 export default router;
