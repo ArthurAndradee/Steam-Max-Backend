2afb7a351e6fe568908bc63422b16aa3f39ee6f4
Arthur Andrade <arthurandrad.23@gmail.com>
2024-08-24 11:05:11 -0300

    Feat: Implemented get route for watchlist

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-
 app.use('/movies', movieRoutes);
 app.use('/videos', videoRoutes);
 app.use('/auth', authRoutes);
--- routes/watchlistRoutes.js
+++ routes/watchlistRoutes.js
@@ -4,6 +4,22 @@ import User from '../models/userModel.js';
 
 const router = express.Router();
 
+router.get('/', authMiddleware, async (req, res) => {
+  try {
+    const userId = req.user.id; // The user ID should be available after the token is verified
+    const user = await User.findById(userId).populate('watchlist'); // Populate the watchlist with movie data
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ watchlist: user.watchlist });
+  } catch (error) {
+    console.error('Error fetching watchlist:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 router.post('/movies', authMiddleware, async (req, res) => {
   const { movie } = req.body;
   const userId = req.user._id;
