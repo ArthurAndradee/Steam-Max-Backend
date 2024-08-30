743a274a827b664257781ccc6233f7e8aa9bf7cb
Arthur Andrade <arthurandrad.23@gmail.com>
2024-08-30 10:19:19 -0300

    Feat: Implemented Register user route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,9 @@
 import express from 'express';
-import { loginUser } from '../controllers/authController.js';
-import User from '../models/userModel.js';
+import { loginUser, registerUser } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
-router.post('/add-profile', async (req, res) => {
-    const { userId, userName, userPicture } = req.body;
-    try {
-      const user = await User.findById(userId);
-      user.profiles.push({ userName, userPicture });
-      await user.save();
-      res.json(user.profiles);
-    } catch (error) {
-      res.status(500).json({ message: 'Server error' });
-    }
-  });
+router.post('/register', registerUser);
 
 export default router;
