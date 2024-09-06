36e075acc0a0086a975c4896c3d6ab9c9f7288ab
arthur andrade <arthurandrad.23@gmail.com>
2024-09-06 20:35:32 -0300

    Fix: Removed unecessary files

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { getMoviesByCategory, getAllMovies } from '../controllers/movieController.js';
+import { getAllMovies } from '../controllers/movieController.js';
 
 const router = express.Router();
 
-router.get('/:category', getMoviesByCategory);
 router.get('/', getAllMovies);
 
 export default router;
