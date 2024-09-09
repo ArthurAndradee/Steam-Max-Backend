00685cb8ac9b28b62d4788af877dbe1e54970afc
arthur andrade <arthurandrad.23@gmail.com>
2024-09-09 12:55:53 -0300

    Fix: Removed unecessary comment

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -2,7 +2,7 @@ import Movie from '../models/movieModel.js';
 
 export const getAllMovies = async (req, res) => {
     try {
-        const allMovies = await Movie.find(); // Get all movies from the database
+        const allMovies = await Movie.find(); 
         res.json(allMovies);
     } catch (error) {
         res.status(500).send('Server error');
