1241e29933fe5f7135980adb743d63bf6049ae49
Arthur Andrade <arthurandrad.23@gmail.com>
2024-09-13 10:26:13 -0300

    Fix: Removed unecessary comments

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/userModel.js'; // Adjust the path to your user model
+import User from '../models/userModel.js';
 
 const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -11,20 +11,16 @@ const authMiddleware = async (req, res, next) => {
   const token = authHeader.split(' ')[1];
 
   try {
-    // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    // Find the user in the database
     const user = await User.findById(decoded.id);
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    // Attach user to request object
     req.user = user;
 
-    // Proceed to the next middleware/route handler
     next();
   } catch (error) {
     console.error('Token verification failed:', error);
